69373acae0b8147999c298cb6d7f4d047eeac264
sqin <sqin@dlbdata.com>
2018-03-31 21:30:50 +0800

    积分页面组件化
    积分页面组件化

--- src/views/addPoint.vue
+++ src/views/addPoint.vue
@@ -1,42 +1,18 @@
 <template>
   <div>
-    <dlbheader></dlbheader>
-
-      <div class="main">
-        <div class="title">
-          <span >党员姓名</span><br>
-          <input type="text"  />
-        </div>
-        <div class="title">
-          <span  class="title">政治学习时间</span><br>
-          <input type="text"  />
-        </div>
-        <div class="title1">
-          <span  class="title">政治学习主要内容</span><br>
-          <input type="text" /><br>
-          <a class="btn" href="#"><span >+</span></a>
-
-        </div>
-        <button>点击增加积分</button>
-
-
-
-
-
-
-    </div>
-
+    <dlbheader :header-title="headerTitle"></dlbheader>
+    <dlbmain></dlbmain>
   </div>
 
 
 </template>
 <script>
 import header from '../components/layout/header.vue'
-import footer from '../components/layout/footer.vue'
+import main1 from '../components/layout/pointmain.vue'
 export default {
 	data(){
 		return {
-
+      headerTitle:"政治学习积分增加"
 
 
 
@@ -47,6 +23,7 @@ export default {
 	,
 	components:{
    'dlbheader':header,
+    'dlbmain':main1
 
 
 	}}
@@ -60,6 +37,6 @@ export default {
   .main .title1 input{width:95%;height: 1.5rem}
   .main .title1 a span{display:block;margin-top: -0.15rem;font-size:0.4rem ;color: #9B0A1A}
   .btn{margin-top: 0.3rem;border:0.01rem solid #B53141 ;display:block;width:0.4rem;height:0.4rem;text-align: center}
-   button{margin-left: 1.2rem;width: 0.7rem;height: 0.5rem;background-color: #B93647 ;width: 1.5rem ;color: #FFFFFF ;border-radius:0.1rem;font-size: 0.18rem}
+   button{margin-left:0.1rem;height: 0.5rem;background-color: #B93647 ;width:93% ;color: #FFFFFF ;border-radius:0.1rem;font-size: 0.18rem}
 
 </style>
